4c56ed8be4bafab6f89c6583c7b177174638fcf7
Pedroso <guilhermepedrosogg@gmail.com>
2022-05-29 20:38:24 -0300

    file

---
+++ codes/prog5.c
@@ -0,0 +1,18 @@
+/*
+  Title: Tree 
+  
+  Author: Guilherme Pedroso
+  Date: May 2022
+  Updated: --
+
+*/
+
+#include <stdio.h>
+
+int main()
+  {
+  printf("\n\t\tTree\n\n\t\t  *\n\t\t ***\n\t\t*****\n\t\t /|\\\n");
+
+  getchar();  
+  return(0);
+  }
