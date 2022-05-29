4a8c258e818a0979dbd9855f0c2f64fc1be6ba1f
Pedroso <guilhermepedrosogg@gmail.com>
2022-05-29 14:29:18 -0300

    file

---
+++ codes/prog4.c
@@ -0,0 +1,28 @@
+/* =============================================================
+  Title: Types modifiers
+  
+  Author: Guilherme Pedroso
+  Date: May 2022 
+  Updated: --
+
+================================================================= */
+
+/* Libraries */
+#include <stdio.h>
+
+/*Main function*/
+int main()
+  {
+  printf("void = %ld bytes\n", sizeof(void));	  
+  printf("char = %ld bytes\n", sizeof(char));  
+  printf("int = %ld bytes\n", sizeof(int));
+  printf("float = %ld bytes\n", sizeof(float));
+  printf("double = %ld bytes\n", sizeof(double));
+
+
+
+  getchar();
+  return(0);
+  } /* end main*/
+
+
