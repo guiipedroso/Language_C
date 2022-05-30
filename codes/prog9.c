50226894d39bad73384dcc8793b95f09f8a51f20
Pedroso <guilhermepedrosogg@gmail.com>
2022-05-29 23:00:46 -0300

    file

---
+++ codes/prog9.c
@@ -0,0 +1,26 @@
+/*
+  Title: Frequency
+  Author: Guilherme Pedroso
+  Date: May 2022
+  Updated: --
+
+*/
+
+/* Libraries */
+#include <stdio.h>
+
+/* Main function*/
+int main()
+  {
+  
+  float pi = 3.141592, 
+        capacitor = 1.0E-6,
+       	resistor = 3.0E3,
+	frequency;
+
+  frequency = (1/(2*pi*resistor*capacitor));
+  
+  printf("%.2f", frequency);
+  getchar();
+  return(0);
+  }
