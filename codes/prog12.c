89ab40279ac3d9906ed8b183214d0e13a7381860
Pedroso <guilhermepedrosogg@gmail.com>
2022-05-31 12:34:12 -0300

    file

---
+++ codes/prog12.c
@@ -0,0 +1,25 @@
+/*
+  Title: ASCII table
+  Nome: Guilherme Pedroso
+  Date: May 2022
+  Updated: --
+
+*/
+
+/* Libraries */
+#include <stdio.h>
+
+/* main function */
+int 
+main()
+  {
+  char caractere;
+
+  printf("Enter the caractere: ");
+  scanf(" %c", &caractere);
+  printf("Caractere: '%c' ==> ASCII: %d\n", caractere, caractere);
+  
+  getchar();
+  return(0);
+  }
+
