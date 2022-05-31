27a670bcfb501e9d2d19c660f0b6b4098924f967
Pedroso <guilhermepedrosogg@gmail.com>
2022-05-31 16:20:29 -0300

    file

---
+++ codes/prog16.c
@@ -0,0 +1,24 @@
+/*
+  Title: Date
+  Author: Guilherme Pedroso
+  Date: May 2022
+  Updated: --
+
+*/
+
+/* Libraries */
+#include <stdio.h>
+
+/* main function */
+int 
+main()
+{
+  short int d, m;
+  long int y;
+
+  printf("Enter the date in format dd/mm/aaaa: ");
+  scanf("%hd/%hd/%ld", &d, &m ,&y);
+  printf("Date ==> %hd/%hd/%ld\n", d,m,y);
+
+  return(0);
+}
