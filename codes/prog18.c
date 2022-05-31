2393439b4672b2060e586e4503a9b698da0147bf
Pedroso <guilhermepedrosogg@gmail.com>
2022-05-31 20:04:13 -0300

    file

---
+++ codes/prog18.c
@@ -0,0 +1,28 @@
+/*
+  Title: Relational operators
+  Author: Guilherme Pedroso
+  Date: May 2022
+  Updated: --
+
+*/
+
+/* Libraries */
+#include <stdio.h>
+
+/* function main */
+int
+main()
+{
+  int p, q;
+  printf("Enter two integers: ");
+  scanf("%d %d", &p, &q);
+
+  printf("P = Q ==> %d\n", p == q);
+  printf("P > Q ==> %d\n", p > q);
+  printf("P >= Q ==> %d\n", p >= q);
+  printf("P < Q ==> %d\n", p < q);
+  printf("P <= Q ==> %d\n", p <= q);
+  printf("P != Q ==>  %d\n", p != q);
+  
+  return(0);
+}
