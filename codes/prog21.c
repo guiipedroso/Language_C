3c84950e997b976ad2013eda18405fc5a4a8356b
Pedroso <guilhermepedrosogg@gmail.com>
2022-05-31 21:07:16 -0300

    file

---
+++ codes/prog21.c
@@ -0,0 +1,28 @@
+/*
+  Title: Basic Salary
+  Author: Guilherme Pedroso
+  Date: May 2022
+  Updated: --
+
+*/
+
+/* Libraries */
+#include <stdio.h>
+
+/* main function */
+int
+main()
+  {
+  float salary;
+  printf("Salary: ");
+  scanf("%f", &salary);
+
+
+  if(salary < 50000)
+    {
+    salary += 2500;
+    }
+  printf("New salary: %.3f\n", salary);
+  
+  return(0);
+  } /* end main */
