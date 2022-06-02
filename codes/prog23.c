54f6288d74fb44b02cd6fc9e811a069eccfee028
Pedroso <guilhermepedrosogg@gmail.com>
2022-06-01 22:04:20 -0300

    file

---
+++ codes/prog23.c
@@ -0,0 +1,24 @@
+/*
+  Title: Salary III (Using "?")
+  Author: Guilherme Pedroso
+  Date: June 2022
+  Updated: --
+  
+*/
+
+/* Libraries */
+#include <stdio.h>
+
+/* main function */
+int 
+main()
+  {
+  float salary;
+
+  printf("Salary: ");
+  scanf("%f", &salary);
+  salary = salary > 1500 ? salary * 1.05 : salary * 1.10;
+  printf("\nNew salary: %.2f\n", salary);
+  
+  return(0);
+  } /* end main */
---
+++ codes/prog24.c
@@ -0,0 +1,27 @@
+/*
+  Title: Caractere 
+  Author: Guilherme Pedroso
+  Date: June 2022
+  Updated: --
+
+*/
+
+/* Libraries */
+#include <stdio.h>
+
+/* main function */
+int
+main()
+{
+  char c1, c2;
+
+  printf("Enter a caractere: ");
+  scanf("%c", &c1);
+  getchar();
+  printf("Enter a caractere: ");
+  scanf("%c", &c2);
+  printf("\nFirst caractere: %c\nSecond caractere: %c\n", c1, c2);
+  
+  getchar();
+  return(0);
+}
