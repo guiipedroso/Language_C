51ed0c2f4d3a9d0b5dda32f26c2d6c4c4ea08121
Pedroso <guilhermepedrosogg@gmail.com>
2022-06-03 09:02:03 -0300

    register

---
+++ codes/prog37.c
@@ -0,0 +1,28 @@
+/*
+  Title: Register
+  Author: Guilherme Pedroso
+  Date: June 2022
+  Updated: --
+
+*/
+
+/* <Libraries> */
+#include <stdio.h>
+
+/* <Main function> */
+int
+main()
+  {
+  unsigned char regist = 0x00;
+
+  printf("Enter a value for the register[00h-FFh]: ");
+  scanf("%hhX", &regist);
+
+  printf("Value: %Xh\n", regist);
+
+  regist = regist ? 0x80 : 0x00;
+
+  printf("Correct value: %Xh\n", regist);
+  getchar();
+  return(0);
+  }
