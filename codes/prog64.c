f580407d37fe6c30b857c11d189e908ee1bf0977
Pedroso <guilhermepedrosogg@gmail.com>
2022-06-14 13:56:27 -0300

    file

---
+++ codes/prog64.c
@@ -0,0 +1,46 @@
+/*
+  Title: Tree 
+  Author: Guilherme Pedroso
+  Date: June 2022
+  Updated: --
+
+*/
+
+/* <Libraries> */
+#include <stdio.h>
+
+/* <input> */
+int 
+input()
+  {
+  int x;
+  printf("Enter a number: ");
+  scanf("%d", &x);
+  printf("\n");
+  return(x); 
+  }
+
+/* <Print loop> */
+void
+loop(int x)
+  {
+  
+  for(int i = 1; i <= x; i++)
+    {	   
+    int j = 1;	    
+    for(int j = 1; j <= i; j++)    	    
+       printf("*");
+      putchar('\n');
+    }
+  }
+
+/* <main function> */
+int
+main()
+  {
+  int value;
+  value = input();
+  loop(value);  
+  getchar();  
+  return(0);
+  }
