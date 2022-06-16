b15f25582558c0a316b166ebbbd8014fd43ce818
Pedroso <guilhermepedrosogg@gmail.com>
2022-06-16 18:59:57 -0300

    file

---
+++ codes/prog73.c
@@ -0,0 +1,62 @@
+/*
+  Title: Skeleton 
+  Author: Guilherme Pedroso de Lima
+:x
+  Updated: --
+
+*/
+
+/* Libraries */
+#include <stdio.h>
+
+/* <Menu> */
+void
+menu()
+  {
+  char op;	  
+
+  do
+    {
+    printf("\n [MENU]\n");
+    printf("\n(m) Math\n");
+    printf("(p) Physical\n");
+    printf("(q) Quit\n");
+
+    printf("\nOption: ");
+    scanf(" %c", &op);
+
+    switch(op)
+      {
+      case 'm':
+      case 'M':
+	{
+        printf("Math\n");
+	break;
+	}
+      case 'p':
+      case 'P':
+	{
+	printf("Physical\n");
+	break;
+	}
+      case 'q':
+      case 'Q':
+	{
+	printf("Exit\n");
+	break;
+	}   
+      default:
+	printf("Invalid\n");
+      }  
+    } while(op != 'q' && op != 'Q');
+  }
+
+/* Main function */
+int
+main(void)
+  {
+  menu();
+
+  getchar();
+  return(0);
+  } /* end main */
