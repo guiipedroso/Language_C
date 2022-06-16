cb67dc4dec7a86a841e1c71d6f83c21692b1c23e
Pedroso <guilhermepedrosogg@gmail.com>
2022-06-16 17:36:47 -0300

    file

---
+++ codes/prog0.c
@@ -0,0 +1,21 @@
+/*
+  Title: Skeleton 
+  Author: Guilherme Pedroso de Lima
+  Date: May 2022
+  Updated: --
+
+
+*/
+
+/* Libraries */
+#include <stdio.h>
+
+/* Main function */
+int
+main(void)
+  {
+
+
+  getchar();
+  return(0);
+  } /* end main */
