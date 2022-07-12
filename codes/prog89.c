7d3215595557e933dc6cd33422857e8e09c6c9a0
Pedroso <guilhermepedrosogg@gmail.com>
2022-07-11 23:58:45 -0300

    file

---
+++ codes/prog89.c
@@ -0,0 +1,26 @@
+/*
+  Title: Procedures (No Profissional)
+  Author: Guilherme Pedroso 
+  Date: July 2022
+
+*/
+
+/* <Libraries> */
+#include <stdio.h>
+
+/* <Procedure> */
+void
+print()
+  {
+  printf("Hello world!\n");
+  }
+
+/* <Main function> */
+int
+main()
+  {
+  print();
+
+  getchar();
+  return(0);
+  } /* <end main> */
