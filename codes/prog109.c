f4b1a0e4a1a497aa586ef9806b3a65843401149b
Pedroso <guilhermepedrosogg@gmail.com>
2022-07-15 18:45:09 -0300

    file

---
+++ codes/prog109.c
@@ -0,0 +1,34 @@
+/*
+  Title: Sum (Macro)   
+  Author: Guilherme Pedroso 
+  Date: July 2022
+  Updated: --
+
+*/
+
+/* <Libraries> */
+#include <stdio.h>
+
+/* <Macros> */
+#define sum(a,b) a+b 
+#define div(c,d) c/d
+
+/* <Prototypes> */
+
+/* <Main function> */
+int
+main()
+  {
+  int result; /* or float */
+  float res1;
+  result = sum(192, 8);
+  printf("%d\n", result);
+ 
+  /* Another example */
+
+  res1 = div((2.45*141),7);
+  printf("%f\n", res1);
+
+  getchar();
+  return(0);
+  } /* <end main> */
