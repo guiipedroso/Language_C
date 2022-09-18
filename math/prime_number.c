7dd81897104eec144875f766e1a256ce1469b440
Guilherme Pedroso <guilhermepedrosogg@gmail.com>
2022-09-18 13:05:16 -0300

    Prime Number

---
+++ math/prime_number.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void prime(int x);
+
+int
+main()
+  {
+  int x, cont = 1;
+  register int i = 2;
+  printf("Entre a number: ");
+  scanf("%d", &x);
+
+  for(i; i <= x; i++)
+    {
+    if(x % i == 0) 
+      {
+      cont++;
+      }
+    }
+  
+  prime(cont);
+  
+  return(0);
+  }
+
+void
+prime(int contt)
+ {
+ if(contt == 2)
+   {
+   printf("Is prime numero\n");
+   }
+ else 
+   {
+   printf("Isn't prime\n");
+   }
+ }
+
